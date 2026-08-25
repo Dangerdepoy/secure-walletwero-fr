6d7dadb9f88194ae4e5e59b5069ef85d03fa4762
Dangerdepoy <124503740+Dangerdepoy@users.noreply.github.com>
2026-08-25 04:12:30 +0000

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,32 +1,3 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-// @ts-nocheck
-
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 export default defineCloudflareConfig({});
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: async () => cache,
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-    },
-  },
-
-  dangerous: {
-    enableCacheInterception: false,
-  },
-};
-
-export default config;
